51c390617b92a8f914d7deb499ac83dcca0ddce6
Araceli Lasso <aracelilasso@amalgama.co>
2025-07-08 17:05:28 -0300

    operadores, booleanos, input

---
+++ 04_input.rb
@@ -0,0 +1,23 @@
+#?Input
+
+puts "Introduce tu edad: "
+edad = gets 
+
+puts "Has introducido el valor: #{edad}"
+
+#? Booleanos
+#Un valor booleano es un tipo de dato que puede ser verdadero (true) o falso (false).
+#Por convencion, si es una variable booleana, al principio del nombre se usa el prefijo "is_" o "has_".
+is_adult = true
+puts is_adult
+
+#? Operacion logica: El resultado siempre sera un booleano.
+#Operadores logicos:
+>
+<
+==
+!=
+>=
+<=
+
+#?Operacion aritmetica: El resultado sera un numero.
