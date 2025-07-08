a61a2f0f9531856544ad71eb39d00141e38c0bd1
Araceli Lasso de la vega <aracelilassodelavega@MacBook-Pro-de-Araceli.local>
2025-07-08 12:56:14 -0300

    mi primer commit

---
+++ 01_practice.rb
@@ -0,0 +1,23 @@
+##Al declarar una variable no es necesario declarar su tipo
+#"Inferencia" El sistema iinterpreta el tipo de dato del contenido de la variable
+#sin la necesidad de hacer el tipo explicito.
+#Texto
+greetings = "Hola, Ruby!"
+#Nos ahorramos sintaxis, de otra forma seria:
+# var greetings:String = "Hola, Ruby!"
+
+puts greetings
+ 
+#Variables: Espacio en la memoria que usaremos para guardar datos.
+
+ greetings = 50
+#Puedo reescribir la variable y, ademas, cambiar el tipo de dato
+ puts greetings
+ 
+#Convencion de nombres: SnakeCase, todo en minuscula y el uso de barra baja para
+#separar palabras. Las variables no oueden empezar con mayuscula ni numero.
+
+#Constante: Comienza con una letra mayuscula, es una espacio en la memoria que
+#usaremos para guardar datos que no cambian. 
+
+
